849b9424097bd6577b0d178031cd9c0308ca2fe2
aupium <guilloualexis83@gmail.com>
2020-08-18 15:31:23 +0200

    first commit

---
+++ src/main/java/com/smwb/serveurManagerWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smwb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class serveurManagerWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(serveurManagerWebBackendApplication.class, args);
+    }
+
+}
